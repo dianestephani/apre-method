2005f84c89988362be2686e498362e8eb41ce692
Diane Stephani <diane.stephani@gmail.com>
2025-10-22 09:51:59 -0700

    cleaned up routine education from paragraph to table

---
+++ apre-method/src/BasicProtocolTable.jsx
@@ -0,0 +1,22 @@
+function BasicProtocolTable() {
+  return (
+    <table className="table table-bordered">
+      <tbody>
+        <tr>
+          <td>APRE 3</td>
+          <td>Strength/Power</td>
+        </tr>
+        <tr>
+          <td>APRE 6</td>
+          <td>Strength/Hypertrophy</td>
+        </tr>
+        <tr>
+          <td>APRE 10</td>
+          <td>Hypertrophy</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default BasicProtocolTable
